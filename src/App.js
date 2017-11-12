0c55b2340358464945eacfb78842988780b2a2c7
bother7 <josephmcha@gmail.com>
2017-11-12 14:37:16 -0500

    export data

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import HealthVisual from './component/HealthVisual'
+import {pub} from './data/hospitalizations.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <p>{pub}</p>
         <HealthVisual />
       </div>
     );
