84d34948dac6bcd22e60e6024b339b524fcd2b5f
bother7 <josephmcha@gmail.com>
2017-11-19 20:47:20 -0500

    logo remove

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import HealthVisual from './component/HealthVisual'
 // import {pub} from './data/hospitalizations.js'
